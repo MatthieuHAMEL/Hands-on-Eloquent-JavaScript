fa0da1af65c7c756a8b47931b02ce2747e42e286
HAMEL <75175897+MatthieuHAMEL@users.noreply.github.com>
2025-01-19 23:09:15 +0100

    Chapter 4 - Exercises (beginning)

--- chapter_4.js
+++ chapter_4.js
@@ -96,6 +96,7 @@ score.visitors = 1; // Still possible to mutate data members
 /////////////////////////////////////////
 // Exercises 
 
+/////////////////////////////////////////
 // I - Range 
 function range(start, end) {
   // Directly initialize the array with the right number of parameters: 
@@ -107,3 +108,72 @@ function range(start, end) {
 }
 
 console.log(range(15, 25));
+
+function sum(arr) {
+  let sum = 0;
+  for (let elt of arr) { // /!\ Do not confuse with "for..in" which doesn't work with arrays.
+    sum += elt;
+  }
+  return sum;
+}
+
+console.log(sum([40, 80, 12]));
+console.log(sum(range(0, 30)));
+
+// Additional assignment :
+// Prec: step != 0
+function range_step(start, end, step) {
+  if (step === undefined) {
+    return range(start, end); // With step of 1 (""optimized"" a little...)
+  }
+
+  let arr = [];
+  for (let i = start; (step > 0) ? i <= end : i >= end; i += step) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+console.log(range_step(1, 10, 2));
+console.log(range_step(5, 2,-1));
+console.log(range_step(15, 25));
+
+/////////////////////////////////////////
+// II - Reversing an array 
+
+// Produce a new array that is reversed.
+function reverseArray(iArr) {
+  let oArr = [];
+  for (let i = iArr.length-1; i >= 0 ; i--) {
+    oArr.push(iArr[i]);
+  }
+  return oArr;
+}
+
+console.log(reverseArray(["Babar", "is", "the", "king", "of", "elephants"]));
+
+function reverseArrayInPlace(ioArr) {
+  // I'll swap ioArr[n] with ioArr[LEN-n-1] for every n in [|0, LEN//2|]
+  // We'll go from 0 to that middle index (LEN//2) below. If there's an odd 
+  // number of parameters, the element is the middle won't change anyway.
+  let middle_idx = (ioArr.length - ioArr.length % 2) / 2;
+
+  for (let i = 0; i < middle_idx; i++) {
+    [ioArr[i], ioArr[ioArr.length - i - 1]] = [ioArr[ioArr.length - i - 1], ioArr[i]]; // swap!
+  }
+}
+
+let arr = [1, 2, 3, 4];
+reverseArrayInPlace(arr);
+console.log(arr);
+
+let arr_2 = [18, 92, 163e51, "Babar", 0, null, {x: 12, y:-150}, ["hey!", true]];
+const arr_2_asString = JSON.stringify(arr_2); // For latter comparison
+
+reverseArrayInPlace(arr_2);
+console.log(arr_2);
+
+reverseArrayInPlace(arr_2);
+console.assert(arr_2_asString === JSON.stringify(arr_2));
+
+
