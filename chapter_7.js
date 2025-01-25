8a473d410380b1581d4d131099ccddaa7868374c
HAMEL <75175897+MatthieuHAMEL@users.noreply.github.com>
2025-01-25 17:56:49 +0100

    Chapter 7 (beginning)

---
+++ chapter_7.js
@@ -0,0 +1,54 @@
+// Chapter 7 : Project
+// The mail-delivery robot
+
+// There are 14 roads in Meadowfield forming a graph 
+const roads = [
+  "Alice's House-Bob's House",   "Alice's House-Cabin",
+  "Alice's House-Post Office",   "Bob's House-Town Hall",
+  "Daria's House-Ernie's House", "Daria's House-Town Hall",
+  "Ernie's House-Grete's House", "Grete's House-Farm",
+  "Grete's House-Shop",          "Marketplace-Farm",
+  "Marketplace-Post Office",     "Marketplace-Shop",
+  "Marketplace-Town Hall",       "Shop-Town Hall"
+];
+
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (from in graph) {
+      graph[from].push(to);
+    } else {
+      graph[from] = [to];
+    }
+  }
+
+  for (let [from, to] of edges.map(r => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
+
+const roadGraph = buildGraph(roads);
+// ...Will look like 
+// {Marketplace: [Post Office, Town Hall, Farm, Shop], 
+//  Alice's House: [Bob's House, Post Office, Cabin], ... }
+
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this; // Do not change the state (no path to the destination)
+    } else {
+      let parcels = this.parcels.map(p => {
+          if (p.place != this.place) return p;
+          return {place: destination, address: p.address};
+        }
+      ).filter(p => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+}
